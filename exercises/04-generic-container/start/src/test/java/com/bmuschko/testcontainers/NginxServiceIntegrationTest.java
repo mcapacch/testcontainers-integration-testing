61f64690008e45c5e39eb91d5ee6dd318124d5da
Marco Cap <marco.capacchietti@edreamsodigeo.com>
2022-01-04 18:17:52 +0100

    generic built container

--- exercises/04-generic-container/start/src/test/java/com/bmuschko/testcontainers/NginxServiceIntegrationTest.java
+++ exercises/04-generic-container/start/src/test/java/com/bmuschko/testcontainers/NginxServiceIntegrationTest.java
@@ -3,20 +3,21 @@ package com.bmuschko.testcontainers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.io.File;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 public class NginxServiceIntegrationTest {
     private NginxService nginxService;
 
+    @Container
+    private final GenericContainer nginxContainer = new GenericContainer(DockerImageName.parse("nginx:1.21.3")).withExposedPorts(80);
+
     @BeforeEach
     public void setUp() {
-        String host = nginx.getHost();
-        Integer port = nginx.getFirstMappedPort();
+        String host = nginxContainer.getHost();
+        Integer port = nginxContainer.getFirstMappedPort();
         String endpointUrl = "http://" + host + ":" + port;
         nginxService = new NginxServiceImpl(endpointUrl);
     }
